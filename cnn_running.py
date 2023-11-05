2d28c6c257358d7c0f06779f59754fc7bed88e77
sijie-han <sijiehan0918@gmail.com>
2023-11-04 23:14:24 -0400

    hi

--- cnn_running.py
+++ cnn_running.py
@@ -6,23 +6,18 @@ import platform
 model = load_model('my_model.h5')
 
 
-def load_single_image(image, img_size=(48, 48)):
-    #images = []
-    #unix_path = path.replace('\\', '/')
-    #path = path if platform.system() == 'Windows' else unix_path
-    # Open image, resize it and convert to grayscale 
+def preprocess_image(image, img_size=(48, 48)):
+    # Resize the image and convert to grayscale 
     img = image.resize(img_size).convert('L') 
     # Convert image to numpy array and normalize (rescale) it
     img = np.array(img) / 255.0
-    
     return img
-# Path to the new image
+
 def fatigue_pred(image):
     # Preprocess the image
-    preprocessed_image = load_single_image(image)
+    preprocessed_image = preprocess_image(image)
 
     # Predict the class with the model
-    prediction = model.predict(preprocessed_image)
-    print(prediction)
-    predicted_class = int(prediction > 0.2)  # Using 0.5 as the threshold
+    prediction = model.predict(np.array([preprocessed_image]))
+    predicted_class = int(prediction > 0.2)  # Using 0.2 as the threshold
     return True if not predicted_class else False
